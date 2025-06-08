3ee18c246038a544f3c3792b9950739105b1edc0
mrgadhvii-os <jaydevswebpannel@gmail.com>
2025-06-08 22:47:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = int(os.environ.get("API_ID", "28201702"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH","31c9bbed9c688b89736d94da7e89653b")
 # ------------------------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7996096550:AAFLQ3ge0iqfm1STzCmpFvXk8cqHFP2re5g")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # ------------------------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "@UGExtractorBot")
 BOT_TEXT = "UG Extractor"
