ecf367f2a6bdb7e78f58eae5847c4647e9351b20
mrgadhvii-os <jaydevswebpannel@gmail.com>
2025-06-08 23:04:30 +0530

    Update func.py

--- Extractor/core/func.py
+++ Extractor/core/func.py
@@ -7,6 +7,41 @@ from Extractor.core import script
 from Extractor.core.mongo.plans_db import premium_users
 
 
+async def get_seconds(time_string):
+    def extract_value_and_unit(ts):
+        value = ""
+        unit = ""
+
+        index = 0
+        while index < len(ts) and ts[index].isdigit():
+            value += ts[index]
+            index += 1
+
+        unit = ts[index:].lstrip()
+
+        if value:
+            value = int(value)
+
+        return value, unit
+
+    value, unit = extract_value_and_unit(time_string.lower())
+
+    if unit == 's':
+        return value
+    elif unit == 'min':
+        return value * 60
+    elif unit == 'hour':
+        return value * 3600
+    elif unit == 'day':
+        return value * 86400
+    elif unit == 'month':
+        return value * 86400 * 30
+    elif unit == 'year':
+        return value * 86400 * 365
+    else:
+        return 0
+
+
 async def subscribe(app, message):
     try:
         update_channel = CHANNEL_ID2
