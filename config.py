fb0ba839491f01c81b5f30897294324267242882
mrgadhvii-os <jaydevswebpannel@gmail.com>
2025-07-23 08:23:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "7743201204"))
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002683859662"))
 
 # //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002529202720")) 
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002741179238")) 
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://ankush23:JYzSGlbjpFezDSWH@cluster0.ulzzd1m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URL = MONGO_URL
