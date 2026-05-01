15d2f050fcee364a3dc71f7dcdb1fe393de4897a
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-01 11:22:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,29 +10,29 @@ API_HASH = os.environ.get("API_HASH","31c9bbed9c688b89736d94da7e89653b")
 # ------------------------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@UGExtractorProBot")
-BOT_TEXT = "𝗨 𝗚 ♡ Exᴛʀᴀᴄᴛᴏʀ"
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@CR_EXTRACTORBOT")
+BOT_TEXT = "CR EXTRACTOR"
 # ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "7743201204"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7889313105"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002683859662"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003728425738"))
 
 # //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002677611375")) 
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1003799236727")) 
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "")
 DATABASE_URL = MONGO_URL
 # -----------------------------------------------
-WITHOUT_LOGS = int(os.environ.get("WITHOUT_LOGS", "-1002829170533"))
+WITHOUT_LOGS = int(os.environ.get("WITHOUT_LOGS", "-1003990309530"))
 PREMIUM_LOGS = WITHOUT_LOGS
 # -----------------------------------------------
-join = '<a href="https://t.me/ItsUGBots"> 𝗨 𝗚 ♡ Exᴛʀᴀᴄᴛᴏʀ </a>'
+join = '<a href="https://t.me/free_courses_2026"> C R ♡ Exᴛʀᴀᴄᴛᴏʀ </a>'
 # -----------------------------------------------
 
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "ItsUGBOT" #without @
+ADMIN_BOT_USERNAME = "CR_EXTRACTORBOT" #without @
 
-THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/twG550x8/IMG-20250627-030801.jpg")
+THUMB_URL = os.environ.get("THUMB_URL", "https://repgyetdcodkynrbxocg.supabase.co/storage/v1/object/public/images/telegram-1777614425099-fff9b81a.jpg")
 
 
